24eb10148fcb84ba3c73cc86d3ee56291915e2d1
jeffch19 <17jcummings@gmail.com>
2023-12-13 20:45:57 -0500

    need to add database

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'rootroot',
-  database: 'your_database',
+  database: '',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
